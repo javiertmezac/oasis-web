a639148f13b88d5484a089860b29680a31789efc
Javier T Meza C <javiermeza.cazarez@gmail.com>
2021-09-29 20:17:34 -0700

    WIP | clients

--- src/app/clients/client.component.ts
+++ src/app/clients/client.component.ts
@@ -1,22 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IClient } from "./client";
+import { ClientService } from "./client.service"
 
 @Component({
   selector: 'pm-client',
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.css']
 })
-export class ClientComponent {
-  pageTitle: string = 'Empresas';
-  clients: any[] = [
-    {
-      "nombre": "random" 
-    },
-    {
-      "nombre": "random" 
-    }
-  ]
+export class ClientComponent implements OnInit {
 
   private _listFilter = 'value';
+  pageTitle: string = 'Empresas';
+  clients: IClient[] = [];
 
   get listFilter(): string {
     return this._listFilter;
@@ -25,4 +20,12 @@ export class ClientComponent {
   set listFilter(value: string) {
     this._listFilter = value;
   } 
+
+  constructor(private clientService: ClientService) {}
+
+  ngOnInit(): void {
+    console.log("ngOnInit")
+    this.clients = this.clientService.getClients();
+
+  }
 }
