e57080304b9ef55bd999b6ecc3b68ad36bd35421
Javier T Meza C <javiermeza.cazarez@gmail.com>
2021-11-14 22:04:32 -0800

    Added Employees Component

--- src/app/employees/employee.service.ts
+++ src/app/employees/employee.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 import { catchError } from "rxjs/operators";
 import { environment } from "src/environments/environment";
 import { HandleHttpClientError } from "../shared/handle-error";
+import { Employee } from "./employee";
 
 @Injectable({
   providedIn: "root"
@@ -21,9 +22,37 @@ export class EmployeeService {
     .pipe(catchError(this.handleHttpError.handleError))
   }
 
-  getEmployeesWithBlockNumber(): Observable<any> {
-    return this.http.get(`${this.employeesApi}?listBlockNumber=true`)
+  getEmployee(id: number): Observable<Employee> {
+    if(id === 0) {
+      return of(this.newEmployee());
+    }
+    return this.http.get<Employee>(`${this.employeesApi}/${id}`)
     .pipe(catchError(this.handleHttpError.handleError))
+  }
+
+  newEmployee(): Employee {
+    return {
+      employeeId : 0,
+      employeeName: '',
+      tel: '',
+      address: '',
+      note: '',
+      registration: new Date(),
+      blockNumber: '',
+      block: ''
+    }
+  }
+
+  insertEmployee(employee: Employee): Observable<any> {
+    return this.http.post<Employee>(this.employeesApi, employee, {
+      headers: { 'Content-Type': 'application/json'}
+    }).pipe(catchError(this.handleHttpError.handleError))
+  }
+
+  updateEmployee(employee: Employee): Observable<any> {
+    return this.http.put<Employee>(this.employeesApi, employee, {
+      headers: { 'Content-Type': 'application/json'}
+    }).pipe(catchError(this.handleHttpError.handleError))
   }
 
   deleteEmployee(employeeId: number): Observable<any> {
