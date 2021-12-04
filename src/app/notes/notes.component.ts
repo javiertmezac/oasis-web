f72f82c2af746d713faeb48856938c697bd84189
Javier T Meza C <javiermeza.cazarez@gmail.com>
2021-12-04 00:46:33 -0800

    added delete note from note.component.ts

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -15,15 +15,35 @@ export class NotesComponent implements OnInit {
   constructor(private noteService: NoteService) { }
 
   ngOnInit(): void {
+    this.loadNotes();
+  }
+
+  loadNotes() : void {
     this.noteService.getNotes().subscribe({
       next: response => this.notesList =response.notesResponse,
       error: err => {
         const emptyNotesError = "Could not fetch Notes";
         if (!err.includes(emptyNotesError)) {
           this.errorMessage = err
+        } else {
+          this.notesList = [];
         }
       }
-    })
+    });
+  }
+
+  deleteNote(noteId: number): void {
+    this.noteService.getNote(noteId).subscribe({
+      next: response => {
+        if(confirm(`Seguro de proceder con el borrado de la nota "${response.note}" ?`)) {
+          this.noteService.deleteNote(noteId).subscribe({
+            next: () => this.loadNotes(),
+            error: err => this.errorMessage = err
+          })
+        }
+      },
+      error: err => this.errorMessage = err
+    });
   }
 
 }
