ff54c7d10ec0a35e1a23694d9eae1a2359498a05
Javier T Meza C <javiermeza.cazarez@gmail.com>
2021-12-14 00:20:24 -0800

    Feature: block edit on employee (#33)
    * Display block to edit the value
    
    * added delete block functionality
    
    * bumped to 1.2.0

--- src/app/block/block-edit/block-edit.component.ts
+++ src/app/block/block-edit/block-edit.component.ts
@@ -27,25 +27,47 @@ export class BlockEditComponent implements OnInit {
     const employeeId = Number(this.route.snapshot.paramMap.get('idEmpleado'))
     this.getEmployee(employeeId);
 
+    const blockId = Number(this.route.snapshot.paramMap.get('idBloque'))
+    this.getBlock(blockId);
+
   }
 
   ngOnInit(): void {
     this.blockForm = this.fb.group({
       letter: ['', [Validators.required, Validators.maxLength(10)]],
       startNumber: ['', Validators.required],
-      endNumber: ['', Validators.required]
+      endNumber: ['', Validators.required],
+      blockNextNumber: ''
     });
   }
 
-  saveBlock():void {
-    if(this.blockForm.valid) {
-      const b = {...this.block, ...this.blockForm.value}
-      b.employeeId = this.employee.employeeId;
+  saveBlock(): void {
+    if (this.blockForm.valid) {
+      if (this.blockForm.dirty) {
 
-      this.blockService.insertBlock(b).subscribe({
-        next: () => this.onSaveComplete(),
-        error: err => this.errorMessage = err
-      })
+        const b = { ...this.block, ...this.blockForm.value }
+        b.employeeId = this.employee.employeeId;
+
+        if (b.blockId == 0) {
+
+          this.blockService.insertBlock(b).subscribe({
+            next: () => this.onSaveComplete(),
+            error: err => this.errorMessage = err
+          });
+
+        } else {
+          // update
+          b.nextBlockNumber = b.blockNextNumber;
+          this.blockService.updateBlock(b).subscribe({
+            next: () => this.onSaveComplete(),
+            error: err => this.errorMessage = err
+          });
+        }
+
+      } else {
+        this.onSaveComplete();
+      }
+
     } else {
       this.errorMessage = "Corregir Errores de Validación";
     }
@@ -58,9 +80,49 @@ export class BlockEditComponent implements OnInit {
     });
   }
 
+  getBlock(blockId: number) {
+    this.blockService.getBlock(blockId).subscribe({
+      next: response => this.displayBlockData(response),
+      error: err => this.errorMessage = err
+    });
+  }
+
+  displayBlockData(block: Block) {
+    if(this.blockForm) {
+      this.blockForm.reset();
+    }
+
+    this.block = block;
+
+    if (this.block.blockId != 0) {
+      this.pageTitle = "Editar Bloque"
+    }
+
+    this.blockForm.patchValue({
+      letter: this.block.letter,
+      startNumber: this.block.startNumber,
+      endNumber: this.block.endNumber,
+      blockNextNumber: this.block.nextBlockNumber != '' ? this.block.nextBlockNumber : ''
+    });
+
+  }
+
   onSaveComplete():void {
     this.blockForm.reset();
     this.router.navigateByUrl('/empleados');
+  }
+
+  deleteEmployeeBlock(): void {
+    if (this.block.blockId == 0) {
+      this.errorMessage = "No hay bloque a eliminar";
+    }
+
+    if(confirm(`Seguro de proceder con el borrado del Bloque?`)) {
+      this.blockService.deleteBlock(this.block.blockId).subscribe({
+        next: () => this.onSaveComplete(),
+        error: err => this.errorMessage = err
+      });
+    }
   }
 
 }
--- src/app/block/block-edit/block.service.ts
+++ src/app/block/block-edit/block.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 import { catchError } from "rxjs/operators";
 import { HandleHttpClientError } from "src/app/shared/handle-error";
 import { environment } from "src/environments/environment";
@@ -31,7 +31,37 @@ export class BlockService {
   }
 
   getBlock(id: number): Observable<Block> {
+    if (id == 0) {
+      return of(this.newBlock());
+    }
     return this.http.get<Block>(`${this.blocksUri}/${id}`)
+    .pipe(catchError(this.httpError.handleError));
+  }
+
+  newBlock(): Block {
+    return {
+      blockId: 0,
+      employeeId: 0,
+      nextBlockNumber: '',
+      letter: '',
+      startNumber: 0,
+      endNumber: 0
+    }
+  }
+
+  deleteBlock(blockId: number): Observable<any> {
+    return this.http.delete(`${this.blocksUri}/${blockId}`)
+    .pipe(catchError(this.httpError.handleError))
+  }
+
+  updateBlock(block: Block): Observable<any> {
+    const httpHeaders = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+
+    return this.http.put<Block>(this.blocksUri, block, httpHeaders)
     .pipe(catchError(this.httpError.handleError));
   }
 
